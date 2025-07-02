542c478deb8393b67e7e4af89e5ea665a783e9fc
Prichard-1 <genarodube8@gmail.com>
2025-07-02 20:18:52 +0200

    add sql file

--- task_2.sql
+++ task_2.sql
@@ -34,6 +34,37 @@ CREATE TABLE Order_Details (
     book_id INT,
     quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY(book_id) REFERENCES BOOKS(BOOK_ID)
+    -- Create authors table
+CREATE TABLE authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
 );
+
+-- Create books table
+CREATE TABLE books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+-- Create customers table
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+-- Create orders table
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (book_id) REFERENCES books(book_id)
+);
+
 
