ef94e0415f3c11aa54466e1fddb169ee09dd1cb7
Prichard-1 <genarodube8@gmail.com>
2025-07-02 13:33:54 +0200

    add alx book store file.

---
+++ alx_book_store.sql
@@ -0,0 +1,51 @@
+--1.create database
+CREATE DATABASE IF MOT EXIST alx_book_store;
+
+--2.use the database
+USE alx_book_store;
+
+--3. create the authors tables
+
+CREATE TABLE Authors(
+	author_id INT PRIMARY KEY,
+	author_name varchar(215)
+);
+
+--4 create books table
+ CREATE TABLE Books(
+	book_id INT PRIMARY KEY,
+	title VARCHAR(130),
+	author_id INT,
+	price DOUBLE,
+	publication_date DATE,
+	FOREIGN KEY(author_id) REFERENCES Authors(author_id)
+);
+
+--5 create customer table
+
+CREATE TABLE Customers(
+	customer_id INT PRIMARY KEY,
+	Customer_name VARCHAR(215),
+	email VARCHAR(215),
+	adress TEXT
+);
+--6. create orders table
+
+CREATE TABLE Orders(
+	order_id  INT PRIMARY KEY,
+	customer_id INT,
+	order_date DATE,
+	FOREIGN KEY (customer_id) REFERENCE Customers(customer_id)
+);
+--7. create order details
+
+CREATE TABLE Order_details(
+	orderdetailid INT PRIMARY KEY,
+	order_id INT,
+	Book_id INT
+	quantity DOUBLE,
+        FOREIGN KEY(order_id) REFRERENCE Orders(order_id),
+        FOREIGN KEY(book_id)  REFERENCE Books(book_id)
+);
+
+
