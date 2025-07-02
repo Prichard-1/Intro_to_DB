fab678984704dbfab5b68e47a94effb98e81651f
Prichard-1 <genarodube8@gmail.com>
2025-07-02 14:13:13 +0200

    add alx book store file.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -37,15 +37,15 @@ CREATE TABLE Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
---7. create order details
-
-CREATE TABLE Order_details(
-	orderdetailid INT PRIMARY KEY,
-	order_id INT,
-	Book_id INT
-	quantity DOUBLE,
-        FOREIGN KEY(order_id) REFERENCES Orders(order_id),
-        FOREIGN KEY(book_id)  REFERENCES Books(book_id)
+--7. create order dettails(
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
+
 
 
