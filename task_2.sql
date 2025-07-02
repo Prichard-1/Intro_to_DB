79aaacb07b50834c507dcaa8cccfedd2e202a7e1
Prichard-1 <genarodube8@gmail.com>
2025-07-02 20:50:29 +0200

    add sql file

--- task_2.sql
+++ task_2.sql
@@ -1,42 +1,48 @@
--- Create authors table
-CREATE TABLE authors (
-    author_id INT PRIMARY KEY,
-    author_name VARCHAR(215)
+--1.create database
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+--2.use the database
+USE alx_book_store;
+
+--3. create the authors tables
+
+CREATE TABLE Authors(
+	author_id INT PRIMARY KEY,
+	author_name varchar(215)
 );
 
--- Create books table
-CREATE TABLE books (
-    book_id INT PRIMARY KEY,
-    title VARCHAR(130),
-    author_id INT,
-    price DOUBLE,
-    publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+--4 create books table
+ CREATE TABLE Books(
+	book_id INT PRIMARY KEY,
+	title VARCHAR(130),
+	author_id INT,
+	price DOUBLE,
+	publication_date DATE,
+	FOREIGN KEY(author_id) REFERENCES Authors(author_id)
 );
 
--- Create customers table
-CREATE TABLE customers (
+--5 create customer table
+CREATE TABLE Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
--- Create orders table
-CREATE TABLE orders (
+--6. create orders table
+CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Create order_details table
-CREATE TABLE order_details (
-    order_detail_id INT PRIMARY KEY,
+--7. create order dettails(
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
-
